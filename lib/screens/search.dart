ce21e8c6951b820fa366bb106cf9e66f432ab6a9
wael4code <121891714+wael4code@users.noreply.github.com>
2023-05-22 00:52:32 +0200

    edit ui searsh screen

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,6 +1,6 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:insta_s_m_app/share/colors.dart';
+
 
 class Search extends StatefulWidget {
   const Search({Key? key}) : super(key: key);
@@ -13,10 +13,14 @@ class _SearchState extends State<Search> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: mobileBackgroundColor,
       appBar: AppBar(
-        title: const Text("Search Screen"),
+        backgroundColor: mobileBackgroundColor,
+        title: TextFormField(
+            decoration:
+                const InputDecoration(labelText: 'Search for a user...'),
+          ),
       ),
-      
     );
   }
 }
